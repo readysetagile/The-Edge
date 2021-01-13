07ab1e0d53f2d2538b1731d351c55be4fa876053
danield33 <danieldawit33@gmail.com>
2021-01-12 19:35:23 -0500

    Add Signup page
    Colors and style can be changed but the color palette was generated randomly. Functionality not added

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+
 
 export default function App () {
+  const [text, setText] = useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.logo}>The Edge</Text>
+      <View style={styles.inputView} >
+        <TextInput
+          style={styles.inputText}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={text => setText({email: text})} />
+      </View>
+      <View style={styles.inputView} >
+        <TextInput
+          style={styles.inputText}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry
+          onChangeText={text => setText({password: text})} />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.forgot}>Forgot Password?</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginButton}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text style={styles.loginText}>Signup</Text>
+      </TouchableOpacity>
+
+
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f92a82',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#d6d5b3",
+    marginBottom: 40
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#ed7b84",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  inputText: {
+    height: 50,
+    color: "white"
+  },
+  forgot: {
+    color: "white",
+    fontSize: 15
+  },
+  loginButton: {
+    width: "80%",
+    backgroundColor: "#7eb77f",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  },
+  loginText: {
+    color: "white"
+  }
 });
