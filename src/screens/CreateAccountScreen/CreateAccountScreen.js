fc4123ac9e96cabaa8b357b37d0604df6c96ffbd
Daniel <danieldawit33@gmail.com>
2021-01-25 21:50:58 -0500

    Add CreateAccount Page

--- src/screens/CreateAccountScreen/CreateAccountScreen.js
+++ src/screens/CreateAccountScreen/CreateAccountScreen.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import styles from './styles';
-import {SafeAreaView, ScrollView, TextInput} from "react-native";
+import {TextInput} from "react-native";
 
 
 export default function CreateAccountScreen ({navigation}) {
     return (
-        <View style={styles.view}>
-            <ScrollView>
+        <View style={styles.container}>
+
+            <Text style={styles.text}>Create Account</Text>
+
+            <View style={styles.contentContainer}>
+
+                <Text style={styles.inputDescription}>Display Name</Text>
+                <View style={styles.inputView}>
+                    <TextInput style={styles.inputText}
+                    placeholder="Username"
+                    placeholderTextColor="white"/>
+                </View>
+
+                <Text style={styles.inputDescription}>Email</Text>
+                <View style={styles.inputView}>
+                    <TextInput style={styles.inputText}
+                    placeholder="example@email.com"
+                    placeholderTextColor="white"/>
+                </View>
+
+                <Text style={styles.inputDescription}>Password</Text>
+                <View style={styles.inputView}>
+                    <TextInput style={styles.inputText}
+                    placeholder="Password"
+                    secureTextEntry
+                    placeholderTextColor="white"/>
+                </View>
+
+                <Text style={styles.inputDescription}>Verify Password</Text>
+                <View style={styles.inputView}>
+                    <TextInput style={styles.inputText}
+                    placeholder="Repeat Password"
+                    secureTextEntry
+                    placeholderTextColor="white"/>
+                </View>
+
+            </View>
+
+        </View>
+    );
+}
+
+/*
+<ScrollView>
                 <Text style={styles.text}>
                     Create Account
                 </Text>
@@ -33,6 +75,4 @@ export default function CreateAccountScreen ({navigation}) {
                 </View>
 
             </ScrollView>
-        </View>
-    );
-}
+ */
--- src/screens/CreateAccountScreen/styles.js
+++ src/screens/CreateAccountScreen/styles.js
@@ -3,9 +3,50 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 20,
+        alignItems: 'center',
+        backgroundColor: "#f92a82"
     },
-    scrollView: {
+    contentContainer:{
+        backgroundColor: "#2adaf9",
+        height: '80%',
+        width: '80%',
+        marginTop: 20,
+        shadowColor: "black",
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 30
+    },
+    text: {
+        fontSize: 42,
+        marginTop: 20,
+        color: "#d6d5b3",
+        fontWeight: 'bold'
+    },
+
+    inputView: {
+        width: "80%",
+        backgroundColor: "#ed7b84",
+        borderRadius: 25,
+        height: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        padding: 20
+    },
+    inputText: {
+        height: 50,
+        color: "white"
+    },
+    inputDescription:{
+        fontSize: 42,
+        marginTop: 20,
+        color: "#f92a89",
+        fontWeight: 'bold'
+    }
+
+});
+
+/*
+scrollView: {
         backgroundColor: "#ed7b84",
     },
     view:{
@@ -43,5 +84,4 @@ export default StyleSheet.create({
         fontWeight: "bold",
         padding: 10,
     }
-
-});
+ */
