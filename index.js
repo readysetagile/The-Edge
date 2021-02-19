d28ccc2072accc2bd0e55f057df0d05051d3c424
daniel <danieldawit33@gmail.com>
2021-02-18 20:47:11 -0500

    Update code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
-import {AppRegistry} from "react-native-web";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
