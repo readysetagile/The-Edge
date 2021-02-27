190c0b8d816660717dc456bcaaabfe6f7f96624c
Daniel <danieldawit33@gmail.com>
2021-02-26 19:31:54 -0500

    Removed Files

--- src/Components/ProfileImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {View} from "react-native";
-
-const ProfileImage = (props) => {
-    const {children, style} = props;
-    return (
-        <View {...this.props} style={style}></View>
-    )
-}
