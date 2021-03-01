4a1ab68061a980e8082f148d080de05a50f902d2
Daniel <danieldawit33@gmail.com>
2021-03-01 10:55:58 -0500

    Add question Creation

--- src/firebase/Util.js
+++ src/firebase/Util.js
@@ -3,9 +3,9 @@
  * @param string [xxxx-xxxx-xxxx-xxxx] replaces any 'x' found in this string with a random character. Otherwise returns a uniuqe string
  * @returns {string} a randomly generated unique ID
  */
-export function createUUID(string) {
+export function createUUID(string='xxxx-xxxx-xxxx-xxxx') {
     let dt = new Date().getTime();
-    const uuid = (string == null ? 'xxxx-xxxx-xxxx-xxxx' : string).replace(/[xy]/g, function (c) {
+    const uuid = string.replace(/[xy]/g, function (c) {
         const r = (dt + Math.random() * 16) % 16 | 0;
         dt = Math.floor(dt / 16);
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
--- src/screens/TeamRoutes/TeamQuestions/QuestionCreationPage.js
+++ src/screens/TeamRoutes/TeamQuestions/QuestionCreationPage.js
@@ -1,12 +1,51 @@
 import React, {Component} from 'react';
-import {View, Text} from "react-native";
+import {View, Text, ScrollView, TouchableOpacity, TextInput} from "react-native";
+import {globalStyles} from "../../GlobalStyles";
+import colors from "../../styles";
+import {Ionicons} from "@expo/vector-icons";
 
 class QuestionCreationPage extends Component {
 
+    state = {
+        questions:[]
+    }
+
+    generateNewQuestion(){
+
+        return(
+            <View style={{backgroundColor: 'white', padding: 50}} key={this.state.questions.length}>
+                <TextInput
+                    style={{padding: 20}}
+                    fontSize={20}
+                placeholder={"Question"}
+                placeholderTextColor={"lightgrey"}/>
+                <Text>hi</Text>
+            </View>
+        )
+
+    }
+
+    addQuestion(){
+
+        let question = this.generateNewQuestion();
+        this.setState({questions: [...this.state.questions, question]})
+
+    }
+
     render() {
         return (
-            <View style={{flex: 1, padding: 50}}>
-                <Text>hi</Text>
+            <View style={{...globalStyles.container, backgroundColor: colors.background}}>
+
+                <ScrollView ref={ref => {this.scrollView = ref}} onContentSizeChange={() => this.scrollView.scrollToEnd({animated: true})}>
+
+                    {this.state.questions}
+
+                </ScrollView>
+
+                <TouchableOpacity style={globalStyles.newButton}>
+                    <Ionicons name={'add'} size={35} style={{alignSelf: 'center', color: 'gold'}} onPress={() => this.addQuestion()}/>
+                </TouchableOpacity>
+
             </View>
         );
     }
