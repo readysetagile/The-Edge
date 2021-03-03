5bee03c17543fd21f41600739b360476c7b9e4f9
Daniel <danieldawit33@gmail.com>
2021-03-02 19:16:35 -0500

    Add Checkbox questions

--- src/screens/TeamRoutes/TeamQuestions/QuestionCreationPage.js
+++ src/screens/TeamRoutes/TeamQuestions/QuestionCreationPage.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import {ScrollView, TextInput, TouchableOpacity, View, Text} from "react-native";
+import {ScrollView, TextInput, TouchableOpacity, View, Text, Switch} from "react-native";
 import {globalStyles} from "../../GlobalStyles";
 import colors from "../../styles";
 import {Ionicons} from "@expo/vector-icons";
 import DropDownPicker from 'react-native-dropdown-picker';
 import {createUUID} from "../../../firebase/Util";
 import HiddenView from "../../../Components/HiddenView";
+import CheckBox from 'react-native-check-box'
+
 
 class QuestionCreationPage extends Component {
 
@@ -42,8 +44,9 @@ class QuestionCreationPage extends Component {
                         marginTop: 10,
                         borderBottomWidth: 1,
                         borderColor: 'grey',
-                        fontSize: 10,
+                        fontSize: 15,
                     }}
+                    editable={false}
                     placeholder={"Short Answer Text"}
                     placeholderTextColor={"grey"}/>
             </View>
@@ -60,8 +63,9 @@ class QuestionCreationPage extends Component {
                         marginTop: 10,
                         borderBottomWidth: 1,
                         borderColor: 'grey',
-                        fontSize: 10,
+                        fontSize: 15,
                     }}
+                    editable={false}
                     multiline={true}
                     placeholder={"Short Answer Text"}
                     placeholderTextColor={"grey"}/>
@@ -80,27 +84,9 @@ class QuestionCreationPage extends Component {
                     <View style={{flexDirection: 'row', marginTop: 10}} key={index}>
 
                         <Ionicons name={choice.isFilled ? "ellipse" : "ellipse-outline"} size={15} style={{alignSelf: 'center'}}/>
-                        <TextInput style={{borderBottomWidth: 3, borderColor: 'blue', padding: 10, width: '100%'}}
-                                   placeholder={"Option " + (index+1)}
-                                   placeholderTextColor={"grey"}
-                                   multiline={true}
-                                   value={choice.option}
-                                    onChangeText={(val) => {
-                                        let question = this.state.questionInfo[uuid];
-                                        let mc = question.multipleChoice
-                                        mc[index].option = val;
-                                        this.updateQuestion(uuid, "multipleChoice", mc)
-                                    }}/>
+                        {this.generateChoiceOption(choice, uuid, index)}
 
-                        <Ionicons name={"remove-circle-outline"} size={15} style={{alignSelf: 'center'}} onPress={() => {
-                            if(index > 0 || choices.length > 1) {
-                                let question = this.state.questionInfo[uuid];
-                                let multiChoices = question.multipleChoice;
-                                multiChoices.splice(index, 1)
-                                question.multipleChoice = multiChoices;
-                                this.updateQuestion(uuid, "multipleChoice", multiChoices);
-                            }
-                        }}/>
+                        {this.generateDeleteSymbol(choices, uuid, index)}
 
                     </View>
 
@@ -128,6 +114,82 @@ class QuestionCreationPage extends Component {
 
     }
 
+    generateDeleteSymbol(choices, uuid, index){
+
+        return(
+            <Ionicons name={"remove-circle-outline"} size={15} style={{alignSelf: 'center'}} onPress={() => {
+                if(index > 0 || choices.length > 1) {
+                    let question = this.state.questionInfo[uuid];
+                    let multiChoices = question.multipleChoice;
+                    multiChoices.splice(index, 1)
+                    question.multipleChoice = multiChoices;
+                    this.updateQuestion(uuid, "multipleChoice", multiChoices);
+                }
+            }}/>
+        )
+
+    }
+
+    generateChoiceOption(choice, uuid, index){
+
+        return(
+            <TextInput style={{borderBottomWidth: 3, borderColor: 'blue', padding: 10, width: '100%'}}
+                       placeholder={"Option " + (index+1)}
+                       placeholderTextColor={"grey"}
+                       multiline={true}
+                       value={choice.option}
+                       onChangeText={(val) => {
+                           let question = this.state.questionInfo[uuid];
+                           let mc = question.multipleChoice
+                           mc[index].option = val;
+                           this.updateQuestion(uuid, "multipleChoice", mc)
+                       }}/>
+        );
+
+    }
+
+    generateCheckBoxes(choices, uuid){
+
+        return (
+
+            <View>
+
+                {choices?.map((choice, index) => (
+
+                    <View style={{flexDirection: 'row', marginTop: 10}} key={index}>
+
+                        <CheckBox isChecked={choice.isChecked} onClick={() => {}}/>
+                        {this.generateChoiceOption(choice, uuid, index)}
+
+                        {this.generateDeleteSymbol(choices, uuid, index)}
+
+                    </View>
+
+                ))}
+
+                <TouchableOpacity onPress={() => this.updateQuestion(uuid, "multipleChoice", [...choices, {isFilled: false}])}>
+
+                    <View style={{flexDirection: 'row', marginTop: 10}}>
+
+                        <CheckBox isChecked={false} onClick={() => {}}/>
+                        <TextInput style={{borderBottomWidth: 3, borderColor: 'lightblue', padding: 10, width: '100%'}}
+                                   placeholder={"Add Option"}
+                                   placeholderTextColor={"grey"}
+                                   editable={false}
+                                   onTouchStart={() => this.updateQuestion(uuid, "multipleChoice", [...choices, {isFilled: false}])}
+                        />
+
+                    </View>
+                </TouchableOpacity>
+
+
+            </View>
+
+        )
+
+    }
+
+
     addQuestion() {
 
         let newObj = {
@@ -138,12 +200,14 @@ class QuestionCreationPage extends Component {
             multipleChoice: [{
                 isFilled: false,
                 option: ""
-            }]
+            }],
+            required: false
         }
 
         this.setState({questionInfo: {...this.state.questionInfo, [createUUID('xxxx')]: newObj}})
 
     }
+
 
     render() {
         return (
@@ -163,7 +227,8 @@ class QuestionCreationPage extends Component {
                                     padding: 50,
                                     marginTop: 5,
                                     borderWidth: 2,
-                                    borderColor: 'black'
+                                    borderColor: (values.required ? 'blue' : 'black'),
+                                    borderRadius: 5
                                 }} key={question[0]}>
                                     <TextInput
                                         style={{
@@ -171,7 +236,7 @@ class QuestionCreationPage extends Component {
                                             marginBottom: 5,
                                             borderBottomWidth: 2,
                                             borderColor: 'grey',
-                                            backgroundColor: 'lightgrey',
+                                            backgroundColor: 'lightgrey'
                                         }}
                                         value={question.question}
                                         multiline={true}
@@ -188,7 +253,8 @@ class QuestionCreationPage extends Component {
                                                     items={[
                                                         {label: "Short Answer", value: "shortAnswer"},
                                                         {label: "Long Answer", value: "longAnswer"},
-                                                        {label: "Multiple Choice", value: "multipleChoice"}
+                                                        {label: "Multiple Choice", value: "multipleChoice"},
+                                                        {label: "Check Boxes", value: "checkBoxes"}
                                                     ]} onChangeItem={item => {
                                         this.updateQuestion(question[0], 'type', item.value)
                                     }}/>
@@ -203,6 +269,10 @@ class QuestionCreationPage extends Component {
 
                                     <HiddenView hide={values.type !== 'multipleChoice'}>
                                         {this.generateMultipleChoice(values.multipleChoice, question[0])}
+                                    </HiddenView>
+
+                                    <HiddenView hide={values.type !== 'checkBoxes'}>
+                                        {this.generateCheckBoxes(values.multipleChoice, question[0])}
                                     </HiddenView>
 
                                 </View>
