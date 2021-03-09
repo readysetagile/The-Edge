a17e0c2cdba2545a4e6d23d0c8aae3bed1cbc615
Daniel <danieldawit33@gmail.com>
2021-03-08 21:12:43 -0500

    Add Color slier and name setter to folders

--- src/screens/TeamRoutes/Drills/DrillsList.js
+++ src/screens/TeamRoutes/Drills/DrillsList.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
-import {Text, TextInput, TouchableOpacity, View} from "react-native";
+import {Image, Text, TextInput, TouchableOpacity, View, Dimensions} from "react-native";
 import {globalStyles} from "../../GlobalStyles";
 import colors from "../../styles";
 import NewButton from "../../../Components/NewButton";
 import {connectActionSheet} from '@expo/react-native-action-sheet'
 import {FontAwesome, Ionicons} from "@expo/vector-icons";
-import {ScrollView} from "react-native-web";
-
+import {Menu, MenuOption, MenuOptions, MenuTrigger} from 'react-native-popup-menu';
+import styles from "../../LoginRoutes/ProfileScreen/styles";
+import {
+    SliderHuePicker,
+    SliderSaturationPicker,
+    SliderValuePicker,
+} from 'react-native-slider-color-picker';
+import tinycolor from 'tinycolor2';
 
 class DrillsList extends Component {
 
     state = {
-        drills:[]
+        drills:[],
     }
 
     constructor(props) {
@@ -22,16 +28,67 @@ class DrillsList extends Component {
     }
 
     generateItem(itemType, name, key, onPress){
-
+        const {
+            width,
+        } = Dimensions.get('window');
         return(
 
-            <TouchableOpacity style={{flexDirection: 'row', padding: 15,
-                borderBottomWidth: 2, borderTopWidth: 2, borderColor: 'grey'}} key={key}>
+            <Menu key={key}>
 
-                <FontAwesome name={itemType === 'folder' ? 'folder' : 'file'} size={24} color="gold" style={{alignSelf: 'center'}} onPress={() => onPress()}/>
-                <Text style={{alignSelf: 'center', paddingLeft: 10, fontSize: 20}}>{name}</Text>
+                <MenuTrigger customStyles={{backgroundColor: 'red'}} triggerOnLongPress={true} onAlternativeAction={() => console.log(2)}>
+                    <View style={{flexDirection: 'row', padding: 15,
+                        borderBottomWidth: 2, borderTopWidth: 2, borderColor: 'grey'}}>
+                        <FontAwesome name={itemType === 'folder' ? 'folder' : 'file'} size={24} color="gold" style={{alignSelf: 'center'}} onPress={() => onPress()}/>
+                        <Text style={{alignSelf: 'center', paddingLeft: 10, fontSize: 20}}>{name}</Text>
+                    </View>
+                </MenuTrigger>
+                <MenuOptions>
+                    <MenuOption>
 
-            </TouchableOpacity>
+                        <View style={{padding: 15, flexDirection: 'row', flex: 1}}>
+                            <Text style={{alignSelf: 'center', padding: 2, fontSize: 20}}>Name: </Text>
+                            <TextInput
+                                style={{
+                                    padding: 5,
+                                    borderWidth: 2,
+                                    borderColor: 'grey',
+                                    flex: 1
+                                }}
+                            placeholder={name}
+                            />
+                        </View>
+
+                    </MenuOption>
+                    <MenuOption>
+
+                        <View style={{marginHorizontal: 24, marginTop: 20, height: 12, flex: 1}}>
+                            <SliderHuePicker
+                                ref={view => {this.sliderHuePicker = view;}}
+                                oldColor={'red'}
+                                trackStyle={[{height: 12, width: '100%'}]}
+                                thumbStyle={styles.thumb}
+                                useNativeDriver={true}
+                                onColorChange={(colorHsvOrRgb, resType) =>
+                                    this.setState({color: tinycolor(colorHsvOrRgb).toHexString()})}
+                            />
+                        </View>
+                        <View style={{marginHorizontal: 24, marginTop: 20, height: 12, flex: 1}}>
+                            <SliderSaturationPicker
+                                ref={view => {this.sliderSaturationPicker = view;}}
+                                oldColor={this.state.color}
+                                trackStyle={[{height: 12, width: '100%'}]}
+                                thumbStyle={styles.thumb}
+                                useNativeDriver={true}
+                                onColorChange={this.changeColor}
+                                style={{height: 12, borderRadius: 6, backgroundColor: tinycolor({h: tinycolor(this.state.color).toHsv().h, s: 1, v: 1}).toHexString()}}
+                            />
+                        </View>
+
+                    </MenuOption>
+                </MenuOptions>
+
+            </Menu>
+
 
         )
 
@@ -50,8 +107,8 @@ class DrillsList extends Component {
         drills.push(
                 {
                     content:[],
-                    ['color']: 'gold',
-                    ['name']: name
+                    color: 'gold',
+                    name: name
                 }
         );
 
@@ -97,7 +154,7 @@ class DrillsList extends Component {
                 {
                     this.state.drills.map((i, j) => {
                         const itemType = i.hasOwnProperty('content') ? 'folder' : 'file';
-                        const name = i[Symbol.for('name')]
+                        const name = i.name;
                         return this.generateItem(itemType, name, j, () => console.log(1))
                     })
                 }
