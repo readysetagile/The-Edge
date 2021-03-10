f0e3e7e8fdc2f759ca23c71d8f82e41837bd3027
Daniel <danieldawit33@gmail.com>
2021-03-09 21:14:34 -0500

    Add Color changing and name changing tags

--- src/screens/TeamRoutes/Drills/DrillsList.js
+++ src/screens/TeamRoutes/Drills/DrillsList.js
@@ -1,45 +1,82 @@
 import React, {Component} from 'react';
-import {Image, Text, TextInput, TouchableOpacity, View, Dimensions} from "react-native";
+import {Dimensions, Text, TextInput, View} from "react-native";
 import {globalStyles} from "../../GlobalStyles";
 import colors from "../../styles";
 import NewButton from "../../../Components/NewButton";
 import {connectActionSheet} from '@expo/react-native-action-sheet'
-import {FontAwesome, Ionicons} from "@expo/vector-icons";
+import {FontAwesome, MaterialIcons} from "@expo/vector-icons";
 import {Menu, MenuOption, MenuOptions, MenuTrigger} from 'react-native-popup-menu';
 import styles from "../../LoginRoutes/ProfileScreen/styles";
-import {
-    SliderHuePicker,
-    SliderSaturationPicker,
-    SliderValuePicker,
-} from 'react-native-slider-color-picker';
+import {SliderHuePicker, SliderSaturationPicker,} from 'react-native-slider-color-picker';
 import tinycolor from 'tinycolor2';
+import {createUUID} from "../../../firebase/Util";
 
 class DrillsList extends Component {
 
     state = {
-        drills:[],
+        drills: [],
     }
+    editingNameTag = "";
+    tagEditColor = null;
 
     constructor(props) {
         super(props);
         this.state = {
-            drills:[]
+            tags: {},
+            drills: {}
         }
     }
 
-    generateItem(itemType, name, key, onPress){
+    changeTagName(tag, newName){
+        if(newName && tag.name !== newName){
+            const tags = this.state.tags;
+            delete tags[tag.name];
+            tags[newName] = tag;
+            this.setState({tags: tags})
+            this.editingNameTag = ""
+
+        }
+    }
+
+    changeTagColor(tag, newColor){
+        if(newColor && tag.color !== newColor) {
+            this.state.tags[tag.name].color = newColor;
+            this.setState({
+                tags: this.state.tags
+            })
+        }
+
+    }
+
+
+    generateTag(tag, onPress) {
         const {
             width,
         } = Dimensions.get('window');
-        return(
 
-            <Menu key={key}>
+        return (
 
-                <MenuTrigger customStyles={{backgroundColor: 'red'}} triggerOnLongPress={true} onAlternativeAction={() => console.log(2)}>
-                    <View style={{flexDirection: 'row', padding: 15,
-                        borderBottomWidth: 2, borderTopWidth: 2, borderColor: 'grey'}}>
-                        <FontAwesome name={itemType === 'folder' ? 'folder' : 'file'} size={24} color="gold" style={{alignSelf: 'center'}} onPress={() => onPress()}/>
-                        <Text style={{alignSelf: 'center', paddingLeft: 10, fontSize: 20}}>{name}</Text>
+            <Menu key={tag.name} onOpen={() => {
+                this.editingNameTag = tag.name
+                this.tagEditColor = tag.color;
+            }} onClose={() => {
+                this.changeTagName(tag, this.editingNameTag)
+            }}>
+
+                <MenuTrigger triggerOnLongPress={true} onAlternativeAction={() => console.log(2)}>
+                    <View style={{
+                        flexDirection: 'row', padding: 15, flexWrap: 'wrap',
+                        borderBottomWidth: 2, borderTopWidth: 2, borderColor: 'grey'
+                    }}>
+                        <FontAwesome name={'tag'} size={24} color={tag.color} style={{alignSelf: 'center'}}
+                                     onPress={() => onPress()}/>
+                        <Text style={{alignSelf: 'center', paddingLeft: 10, fontSize: 20}}>{tag.name}</Text>
+
+                        <View style={{flex: 1}}>
+                            <MaterialIcons name={tag.contentHidden ? 'arrow-drop-up' : 'arrow-drop-down'}
+                                           size={30} color={'grey'} style={{alignSelf: 'flex-end'}}/>
+                        </View>
+
                     </View>
                 </MenuTrigger>
                 <MenuOptions>
@@ -47,14 +84,18 @@ class DrillsList extends Component {
 
                         <View style={{padding: 15, flexDirection: 'row', flex: 1}}>
                             <Text style={{alignSelf: 'center', padding: 2, fontSize: 20}}>Name: </Text>
-                            <TextInput
+                            <TextInput ref={ref => {this.tagNameInput = ref}}
                                 style={{
                                     padding: 5,
                                     borderWidth: 2,
                                     borderColor: 'grey',
                                     flex: 1
                                 }}
-                            placeholder={name}
+                                placeholder={tag.name}
+                                onChangeText={val => {
+                                    this.editingNameTag = val
+                                }}
+
                             />
                         </View>
 
@@ -63,24 +104,39 @@ class DrillsList extends Component {
 
                         <View style={{marginHorizontal: 24, marginTop: 20, height: 12, flex: 1}}>
                             <SliderHuePicker
-                                ref={view => {this.sliderHuePicker = view;}}
-                                oldColor={'red'}
+                                ref={view => {
+                                    this.sliderHuePicker = view;
+                                }}
+                                oldColor={this.tagEditColor}
                                 trackStyle={[{height: 12, width: '100%'}]}
                                 thumbStyle={styles.thumb}
                                 useNativeDriver={true}
-                                onColorChange={(colorHsvOrRgb, resType) =>
-                                    this.setState({color: tinycolor(colorHsvOrRgb).toHexString()})}
+                                onColorChange={(colorHsvOrRgb, resType) => {
+                                    this.changeTagColor(tag, tinycolor(colorHsvOrRgb).toHexString())
+                                }}
                             />
                         </View>
                         <View style={{marginHorizontal: 24, marginTop: 20, height: 12, flex: 1}}>
                             <SliderSaturationPicker
-                                ref={view => {this.sliderSaturationPicker = view;}}
-                                oldColor={this.state.color}
+                                ref={view => {
+                                    this.sliderSaturationPicker = view;
+                                }}
+                                oldColor={this.state.tags[tag.name].color}
                                 trackStyle={[{height: 12, width: '100%'}]}
                                 thumbStyle={styles.thumb}
                                 useNativeDriver={true}
-                                onColorChange={this.changeColor}
-                                style={{height: 12, borderRadius: 6, backgroundColor: tinycolor({h: tinycolor(this.state.color).toHsv().h, s: 1, v: 1}).toHexString()}}
+                                onColorChange={(colorHsvOrRgb, resType) => {
+                                    this.changeTagColor(tag, tinycolor(colorHsvOrRgb).toHexString())
+                                }}
+                                style={{
+                                    height: 12,
+                                    borderRadius: 6,
+                                    backgroundColor: tinycolor({
+                                        h: tinycolor(this.state.tags[tag.name].color).toHsv().h,
+                                        s: 1,
+                                        v: 1
+                                    }).toHexString()
+                                }}
                             />
                         </View>
 
@@ -94,35 +150,36 @@ class DrillsList extends Component {
 
     }
 
-    createDrill(){
-
-
-
-
-    }
-
-    createFolder(name){
+    createDrill(tag) {
 
         let drills = this.state.drills;
-        drills.push(
-                {
-                    content:[],
-                    color: 'gold',
-                    name: name
-                }
-        );
+        drills[createUUID('xxxxx')] = {
+            tags: []
+        }
 
-        this.setState({drills: drills});
 
     }
 
-    addItem(){
+    createTag(name) {
 
-        const options = ['Create Drill', 'Create Folder', 'Cancel'];
+        let tags = this.state.tags;
+        tags[name] = {
+            color: 'gold',
+            name: name,
+            contentHidden: true
+        }
+
+        this.setState({tags: tags});
+
+    }
+
+    addItem() {
+
+        const options = ['Create Drill', 'Create Tag', 'Cancel'];
         const cancelButtonIndex = 2;
         const actionMap = {
             0: this.createDrill.bind(this),
-            1: this.createFolder.bind(this),
+            1: this.createTag.bind(this),
         }
 
         this.props.showActionSheetWithOptions({
@@ -134,8 +191,13 @@ class DrillsList extends Component {
 
     }
 
+    showTag(tagName) {
+
+
+    }
 
     render() {
+        const displayedTags = [];
         return (
             <View style={{...globalStyles.container, backgroundColor: colors.background}}>
 
@@ -152,10 +214,13 @@ class DrillsList extends Component {
                 </View>
 
                 {
-                    this.state.drills.map((i, j) => {
-                        const itemType = i.hasOwnProperty('content') ? 'folder' : 'file';
-                        const name = i.name;
-                        return this.generateItem(itemType, name, j, () => console.log(1))
+                    Object.entries(this.state.tags).map((i, j) => {
+
+                        const name = i[0];
+                        const content = i[1];
+                        content.name = name;
+                        return this.generateTag(content, () => console.log(1));
+
                     })
                 }
 
---
+++ src/screens/TeamRoutes/Drills/Objects.js
@@ -0,0 +1,6 @@
+export const Tag = {
+    color: "",
+}
+export const Drill = {
+
+}
