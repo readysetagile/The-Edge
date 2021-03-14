d36409e7a7b9382b4825b9d30f7020069e5d4198
Daniel <danieldawit33@gmail.com>
2021-03-14 13:20:19 -0400

    Revert "New Editor"
    This reverts commit 9f41e79aa6214e456ab9cfae58405cac76237bc8.

--- src/screens/TeamRoutes/Drills/NewDrill.js
+++ src/screens/TeamRoutes/Drills/NewDrill.js
@@ -1,17 +1,134 @@
 import React, {useEffect, useState} from "react";
 import {Platform, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import WebViewQuillJS, {WebViewQuillEditor, WebViewQuillViewer} from 'react-native-webview-quilljs'
+import KeyboardSpacer from "react-native-keyboard-spacer";
+import RNDraftView from "react-native-draftjs-editor";
 
+const ControlButton = ({text, action, isActive}) => {
+    return (
+        <TouchableOpacity
+            style={[
+                styles.controlButtonContainer,
+                isActive ? {backgroundColor: "gold"} : {}
+            ]}
+            onPress={action}
+        >
+            <Text>{text}</Text>
+        </TouchableOpacity>
+    );
+};
 
+const EditorToolBar = ({
+                           activeStyles,
+                           blockType,
+                           toggleStyle,
+                           toggleBlockType,}) => {
+    return (
+        <View style={styles.toolbarContainer}>
+            <ControlButton
+                text={"B"}
+                isActive={activeStyles.includes("BOLD")}
+                action={() => toggleStyle("BOLD")}
+            />
+            <ControlButton
+                text={"L"}
+                isActive={activeStyles.includes("LINK")}
+                action={() => toggleStyle("LINK")}
+            />
+            <ControlButton
+                text={"I"}
+                isActive={activeStyles.includes("ITALIC")}
+                action={() => toggleStyle("ITALIC")}
+            />
+            <ControlButton
+                text={"H1"}
+                isActive={blockType === "header-one"}
+                action={() => toggleBlockType("header-one")}
+            />
+            <ControlButton
+                text={"H2"}
+                isActive={blockType === "header-two"}
+                action={() => toggleBlockType("header-two")}
+            />
+            <ControlButton
+                text={"H3"}
+                isActive={blockType === "header-three"}
+                action={() => toggleBlockType("header-three")}
+            />
+            <ControlButton
+                text={"ul"}
+                isActive={blockType === "unordered-list-item"}
+                action={() => toggleBlockType("unordered-list-item")}
+            />
+            <ControlButton
+                text={"ol"}
+                isActive={blockType === "ordered-list-item"}
+                action={() => toggleBlockType("ordered-list-item")}
+            />
+            <ControlButton
+
+                text={"-T-"}
+                isActive={activeStyles.includes("STRIKETHROUGH")}
+                action={() => toggleStyle("STRIKETHROUGH")}
+            />
+        </View>
+    );
+};
+
+const styleMap = {
+    STRIKETHROUGH: {
+        textDecoration: "line-through"
+    }
+};
 
 const NewDrill = () => {
+    const _draftRef = React.createRef();
+    const [activeStyles, setActiveStyles] = useState([]);
+    const [blockType, setActiveBlockType] = useState("unstyled");
+    const [editorState, setEditorState] = useState("");
+
+    const defaultValue =
+        ""//<h1>A Full fledged Text Editor</h1><p>This editor is built with Draft.js. Hence should be suitable for most projects. However, Draft.js Isn’t fully compatible with mobile yet. So you might face some issues.</p><p><br></p><p>This is a simple implementation</p><ul>  <li>It contains <strong>Text formatting </strong>and <em>Some blocks formatting</em></li>  <li>Each for it’s own purpose</li></ul><p>You can also do</p><ol>  <li>Custom style map</li>  <li>Own css styles</li>  <li>Custom block styling</li></ol><p>You are welcome to try it!</p>";
+
+    const editorLoaded = () => {
+        _draftRef.current && _draftRef.current.focus();
+    };
+
+    const toggleStyle = style => {
+        _draftRef.current && _draftRef.current.setStyle(style);
+    };
+
+    const toggleBlockType = blockType => {
+        _draftRef.current && _draftRef.current.setBlockType(blockType);
+    };
+
+    useEffect(() => {
+        /**
+         * Get the current editor state in HTML.
+         * Usually keep it in the submit or next action to get output after user has typed.
+         */
+        setEditorState(_draftRef.current ? _draftRef.current.getEditorState() : "");
+    }, [_draftRef]);
+    //console.log(editorState);
 
     return (
         <SafeAreaView style={styles.containerStyle}>
-            <WebViewQuillJS
-                backgroundColor={"#FAEBD7"}
-                onMessageReceived={console.log}
+            <RNDraftView
+                defaultValue={defaultValue}
+                onEditorReady={editorLoaded}
+                style={{flex: 1}}
+                placeholder={"Add text here..."}
+                ref={_draftRef}
+                onStyleChanged={setActiveStyles}
+                onBlockTypeChanged={setActiveBlockType}
+                styleMap={styleMap}
             />
+            <EditorToolBar
+                activeStyles={activeStyles}
+                blockType={blockType}
+                toggleStyle={toggleStyle}
+                toggleBlockType={toggleBlockType}
+            />
+            {Platform.OS === "ios" ? <KeyboardSpacer/> : null}
         </SafeAreaView>
     );
 };
