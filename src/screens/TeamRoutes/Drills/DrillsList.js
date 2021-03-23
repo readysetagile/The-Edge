ff52803451bc15fd908cf9b3457c2c520b9d6cdb
Daniel <danieldawit33@gmail.com>
2021-03-22 20:54:45 -0400

    Revert "Reverted"
    This reverts commit 693cfdfc70390682c178fb6730758ae0ee8ba885.

--- src/screens/TeamRoutes/Drills/DrillsList.js
+++ src/screens/TeamRoutes/Drills/DrillsList.js
@@ -27,8 +27,6 @@ import styles from './styles';
 import CheckBox from "react-native-check-box";
 import FlatButton from "../../../Components/SubmitButton";
 import {Drill} from "../../../firebase/modules/Drills";
-import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions';
 
 class DrillsList extends Component {
 
@@ -1153,11 +1151,14 @@ class DrillsList extends Component {
     }
 
     sendDrills(){
+
         const member = this.state.memberToAssign;
         for(const drill of this.state.assignedDrills){
             member.addAssignedDrill(this.newDrill(drill));
         }
+
         this.props.navigation.navigate("Members");
+
     }
 
     confirmSend(){
@@ -1165,7 +1166,7 @@ class DrillsList extends Component {
         Alert.alert("Assign Drills", "Are you sure you want to assign these drills?", [
             {
                 text: 'Yes',
-                onPress: this.recieveDrills.bind(this)
+                onPress: this.sendDrills.bind(this)
             },
             {
                 text: "No"
