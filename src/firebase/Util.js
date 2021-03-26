8c0d53664ee1dec7c9e8928cd96fa075422095f1
Daniel <danieldawit33@gmail.com>
2021-03-25 20:10:22 -0400

    Modules installed

--- src/firebase/Util.js
+++ src/firebase/Util.js
@@ -1,3 +1,8 @@
+import {Alert} from "react-native";
+import * as Permissions from 'expo-permissions';
+import * as Linking from 'expo-linking';
+
+
 /**
  * Creates a new unique ID
  * @param string [xxxx-xxxx-xxxx-xxxx] replaces any 'x' found in this string with a random character. Otherwise returns a uniuqe string
@@ -11,6 +16,39 @@ export function createUUID(string = 'xxxx-xxxx-xxxx-xxxx') {
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
+}
+
+export async function hasNotificationPermission(){
+    try {
+        const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+        let finalStatus = existingStatus;
+        // If we don't already have permission, ask for it
+        if (existingStatus !== 'granted') {
+            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+            finalStatus = status;
+        }
+        if (finalStatus === 'granted') return true;
+        if (finalStatus !== 'granted') {
+            Alert.alert(
+                'Warning',
+                'You will not receive reminders if you do not enable push notifications. If you would like to receive reminders, please enable push notifications for Fin in your settings.',
+                [
+                    { text: 'Cancel' },
+                    // If they said no initially and want to change their mind,
+                    // we can automatically open our app in their settings
+                    // so there's less friction in turning notifications on
+                    { text: 'Enable Notifications', onPress: () => Platform.OS === 'ios' ? Linking.openURL('app-settings:') : Linking.openSettings() }
+                ]
+            )
+            return false;
+        }
+    } catch (error) {
+        Alert.alert(
+            'Error',
+            'Something went wrong while check your notification permissions, please try again later.'
+        );
+        return false;
+    }
 }
 
 
