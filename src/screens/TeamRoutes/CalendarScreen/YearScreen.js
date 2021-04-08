1612fd716aa2c9288cb48b1e34fd5ac91ad21873
Daniel <danieldawit33@gmail.com>
2021-04-08 18:38:58 -0400

    Fixed Slow calendar rendering

--- src/screens/TeamRoutes/CalendarScreen/YearScreen.js
+++ src/screens/TeamRoutes/CalendarScreen/YearScreen.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import {View, FlatList, TouchableOpacity, Text, SafeAreaView} from 'react-native'
+import {View, FlatList, TouchableOpacity, Text, SafeAreaView, ScrollView} from 'react-native'
 import {Calendar} from "react-native-calendars";
 import {globalStyles} from "../../GlobalStyles";
 import colors from "../../styles";
 import {firebase} from '../../../firebase/config';
+import {Ionicons} from "@expo/vector-icons";
 
 class YearScreen extends Component {
 
@@ -24,22 +25,23 @@ class YearScreen extends Component {
 
     state = {
         YEARS_FUTURE: 3,
-        YEARS_PAST: 3
+        YEARS_PAST: 3,
+        yearData: null
     }
 
     componentDidMount() {
 
-        firebase.database().ref("GLOBAL_SETTINGS")
-            .child("CALENDAR").once('value', res => {
-            this.setState({
-                YEARS_FUTURE: res.YEARS_FUTURE,
-                YEARS_PAST: res.YEARS_PAST
-            })
-        });
+        // firebase.database().ref("GLOBAL_SETTINGS")
+        //     .child("CALENDAR").once('value', res => {
+        //     this.setState({
+        //         YEARS_FUTURE: res.YEARS_FUTURE,
+        //         YEARS_PAST: res.YEARS_PAST,
+        //     })
+        // });
 
     }
 
-    generateMonth(year, month) {
+    /*generateMonth(year, month) {
 
         const date = new Date(year, month, 1);
 
@@ -57,56 +59,46 @@ class YearScreen extends Component {
                             padding: 10,
                             borderRadius: 10
                         }}
-                        theme={{
-                            calendarBackground: "#F3CCFF",
-                            arrowColor: '#5EA952',
-                            dayTextColor: 'black',
-                            monthTextColor: 'red',
-                            textSectionTitleColor: 'darkgreen',
-                            textDayFontSize: 10,
-
-                            'stylesheet.calendar.main': {
-
-                                monthView: {
-                                    flex: 1,
-                                    backgroundColor: "#F3CCFF"
-                                },
-                                week: {
-                                    flex: 1,
-                                    flexDirection: 'row',
-                                    justifyContent: 'space-around',
-                                },
-                                container: {
-                                    width: '100%',
-                                    height: '100%',
-                                    backgroundColor: "#F3CCFF",
-                                }
-
-                            }
-
-                        }}
+                        // theme={{
+                        //     calendarBackground: "#F3CCFF",
+                        //     arrowColor: '#5EA952',
+                        //     dayTextColor: 'black',
+                        //     monthTextColor: 'red',
+                        //     textSectionTitleColor: 'darkgreen',
+                        //     textDayFontSize: 10,
+                        //
+                        //     'stylesheet.calendar.main': {
+                        //
+                        //         monthView: {
+                        //             flex: 1,
+                        //             backgroundColor: "#F3CCFF"
+                        //         },
+                        //         week: {
+                        //             flex: 1,
+                        //             flexDirection: 'row',
+                        //             justifyContent: 'space-around',
+                        //         },
+                        //         container: {
+                        //             width: '100%',
+                        //             height: '100%',
+                        //             backgroundColor: "#F3CCFF",
+                        //         }
+                        //
+                        //     }
+                        //
+                        // }}
                         disableTouchEvent={true}
                         disabledByDefault={false}
                         current={date}
                         hideArrows={true}
-                        // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-                        // day from another month that is visible in calendar page. Default = false
                         disableMonthChange={true}
-                        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
                         firstDay={0}
-                        // Hide day names. Default = false
                         hideDayNames={true}
-                        // Show week numbers to the left. Default = false
                         showWeekNumbers={false}
-                        // Disable left arrow. Default = false
                         disableArrowLeft={true}
-                        // Disable right arrow. Default = false
                         disableArrowRight={true}
-                        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
                         disableAllTouchEventsForDisabledDays={true}
-                        // Replace default month and year title with custom one. the function receive a date as parameter.
                         renderHeader={this.generateMonthName.bind(this, month)}
-                        // Enable the option to swipe between months. Default = false
                         enableSwipeMonths={false}
                     />
 
@@ -116,6 +108,25 @@ class YearScreen extends Component {
 
         )
 
+    }*/
+
+    generateMonth(month){
+        return (
+            <TouchableOpacity key={month} style={{
+                width: '28%',
+                height: 150,
+                margin: 10,
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+                borderRadius: 15,
+                backgroundColor: "#F3CCFF",
+            }}>
+                <Text style={{fontWeight: '500', fontSize: 25}}>
+                    {this.dates[month]}
+                </Text>
+                <Ionicons name={'calendar-outline'} size={90}/>
+            </TouchableOpacity>
+        )
     }
 
     generateMonthName(monthNum) {
@@ -125,48 +136,65 @@ class YearScreen extends Component {
     }
 
     generateYear = (yearNum) => {
-        const date = yearNum.item.date;
+        const date = yearNum;
         const months = [];
         for (let i = 0; i < 12; i++) {
-            months.push(this.generateMonth(date, i));
+            //months.push(this.generateMonth(date, i));
+            months.push(this.generateMonth(i));
         }
 
         return (
-            <View style={{
-                flex: 1,
-                backgroundColor: 'orange'
-            }}>
-                <Text style={{fontSize: 35, fontWeight: 'bold', left: 20}}>{date}</Text>
                 <View style={{flexWrap: 'wrap', flexDirection: 'row', width: '100%',}}>
                     {months}
                 </View>
-            </View>
         )
 
     }
 
+
+
     generateYears() {
 
-        const data = [];
-        const currDate = new Date();
-        const currentYear = currDate.getFullYear();
-        for (let i = this.state.YEARS_PAST; i >= 0; i--) {
-            data.push({
-                date: currentYear-i
+        const yearNum = new Date().getFullYear();
+        const year = this.generateYear(yearNum);
+        const years = [];
+
+        for(let i = yearNum-this.state.YEARS_PAST; i < yearNum+this.state.YEARS_FUTURE; i++){
+            years.push({
+                year: year,
+                yearNum: i
             })
         }
 
+
         return (
-            
-                <FlatList
-                    style={{backgroundColor: 'red'}}
-                    data={data}
-                    renderItem={(item) => {
-                        console.log(item)
-                        return <Text>hi</Text>
-                    }}
-                    keyExtractor={(item) => item.date.toString()}
-                />
+
+
+            <ScrollView>
+
+                {years.map(i => {
+
+                    return(
+
+                        <View style={{
+                            flex: 1,
+                        }}>
+                            <Text style={{fontSize: 35, fontWeight: 'bold', left: 20}}>{i.yearNum}</Text>
+                            {i.year}
+                        </View>
+
+                    )
+
+                })}
+
+            </ScrollView>
+
+            // <FlatList
+            //     data={data}
+            //     renderItem={this.generateYear}
+            //     keyExtractor={(item) => item.date.toString()}
+            //     extraData={this.state.YEARS_PAST}
+            // />
 
         );
 
