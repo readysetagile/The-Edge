8bf0cec135d454901d3b7fc100be51e170d40c6e
Daniel <danieldawit33@gmail.com>
2021-04-08 16:45:03 -0400

    Disabled Calendar Buttons

--- src/screens/TeamRoutes/CalendarScreen/YearScreen.js
+++ src/screens/TeamRoutes/CalendarScreen/YearScreen.js
@@ -31,70 +31,73 @@ class YearScreen extends Component {
                 height: 150,
                 margin: 10,
             }}>
+                <View pointerEvents={'none'}>
 
-                <Calendar
-                    style={{
-                        padding: 10
-                    }}
-                    theme={{
-                        calendarBackground: "#F3CCFF",
-                        arrowColor: '#5EA952',
-                        dayTextColor: 'black',
-                        monthTextColor: 'red',
-                        textSectionTitleColor: 'darkgreen',
-                        textDayFontSize: 10,
+                    <Calendar
+                        style={{
+                            padding: 10
+                        }}
+                        theme={{
+                            calendarBackground: "#F3CCFF",
+                            arrowColor: '#5EA952',
+                            dayTextColor: 'black',
+                            monthTextColor: 'red',
+                            textSectionTitleColor: 'darkgreen',
+                            textDayFontSize: 10,
 
-                        'stylesheet.calendar.main':{
+                            'stylesheet.calendar.main':{
 
-                            monthView:{
-                                flex: 1,
-                                backgroundColor: "#F3CCFF"
-                            },
-                            week:{
-                                flex: 1,
-                                flexDirection: 'row',
-                                justifyContent: 'space-around',
-                            },
-                            container:{
-                                width: '100%',
-                                height: '100%',
-                                backgroundColor: "#F3CCFF",
+                                monthView:{
+                                    flex: 1,
+                                    backgroundColor: "#F3CCFF"
+                                },
+                                week:{
+                                    flex: 1,
+                                    flexDirection: 'row',
+                                    justifyContent: 'space-around',
+                                },
+                                container:{
+                                    width: '100%',
+                                    height: '100%',
+                                    backgroundColor: "#F3CCFF",
+                                }
+
                             }
 
-                        }
+                        }}
+                        //Button Yes
+                        //'src/calendar/day/basic/index.js'
+                        disableTouchEvent={true}
+                        disabledByDefault={false}
+                        current={date}
+                        //hideExtraDays={false}
+                        hideArrows={true}
+                        // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+                        // day from another month that is visible in calendar page. Default = false
+                        disableMonthChange={true}
+                        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+                        firstDay={0}
+                        // Hide day names. Default = false
+                        hideDayNames={true}
+                        // Show week numbers to the left. Default = false
+                        showWeekNumbers={false}
+                        // Disable left arrow. Default = false
+                        disableArrowLeft={true}
+                        // Disable right arrow. Default = false
+                        disableArrowRight={true}
+                        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+                        disableAllTouchEventsForDisabledDays={false}
+                        // Replace default month and year title with custom one. the function receive a date as parameter.
+                        renderHeader={(dateGiven) => {
+                            return(
+                                <Text style={{fontWeight: '500', fontSize: 25}}>{this.dates[month]}</Text>
+                            )
+                        }}
+                        // Enable the option to swipe between months. Default = false
+                        enableSwipeMonths={false}
+                    />
 
-                    }}
-                    //Button Yes
-                    //'src/calendar/day/basic/index.js'
-                    disableTouchEvent={true}
-                    disabledByDefault={true}
-                    current={date}
-                    //hideExtraDays={false}
-                    hideArrows={true}
-                    // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
-                    // day from another month that is visible in calendar page. Default = false
-                    disableMonthChange={true}
-                    // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
-                    firstDay={0}
-                    // Hide day names. Default = false
-                    hideDayNames={true}
-                    // Show week numbers to the left. Default = false
-                    showWeekNumbers={false}
-                    // Disable left arrow. Default = false
-                    disableArrowLeft={true}
-                    // Disable right arrow. Default = false
-                    disableArrowRight={true}
-                    // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
-                    disableAllTouchEventsForDisabledDays={false}
-                    // Replace default month and year title with custom one. the function receive a date as parameter.
-                    renderHeader={(dateGiven) => {
-                        return(
-                            <Text style={{fontWeight: '500', fontSize: 25}}>{this.dates[month]}</Text>
-                        )
-                    }}
-                    // Enable the option to swipe between months. Default = false
-                    enableSwipeMonths={false}
-                />
+                </View>
 
             </TouchableOpacity>
 
