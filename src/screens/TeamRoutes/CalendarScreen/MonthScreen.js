af770adf72c9ff5dccaec2bfaace0c5e47c047e1
Daniel <danieldawit33@gmail.com>
2021-04-09 20:25:59 -0400

    Update MonthScreen.js

--- src/screens/TeamRoutes/CalendarScreen/MonthScreen.js
+++ src/screens/TeamRoutes/CalendarScreen/MonthScreen.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import {View} from 'react-native'
+import {globalStyles} from "../../GlobalStyles";
+import colors from "../../styles";
+import {Calendar, Agenda} from 'react-native-calendars';
+import EventCalendar from 'react-native-events-calendar'
+
 class MonthScreen extends Component {
     render() {
         return (
-            <View>
-                
+            <View style={[globalStyles.container, {backgroundColor: colors.background}]}>
+
+
+
             </View>
         );
     }
